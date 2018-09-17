274bb13f6a98ceccb7409e354e3bec4e35b5cac8
TwentyTenNathaniel <42868172+TwentyTenNathaniel@users.noreply.github.com>
2018-09-17 09:32:07 -0400

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+album = {
+    'title' : "Ta13oo",
+    'artist' : "Denzel Curry",
+    'genre' : "Hip Hop",
+    'release_date' : "July 27, 2018",
+    'runtime' : "43:20",
+    'tracks' : "13",
+    }
+    
